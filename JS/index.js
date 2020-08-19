5280c906b7082ef79a0fd83ecb8eb6b47f73a28a
Anveshreddy pinnapareddy <anveshreddypinnapareddy802@gmail.com>
2020-08-19 22:13:24 +0530

    Delete index.js

--- JS/index.js
+++
@@ -1,9 +0,0 @@
-function myFunction(x) {
-  x.classList.toggle("change");
-  var x=document.getElementsByClassName('header')[0];
-  if(x.style.display==="block")
-  		x.style.display="none";
-  else{
-  	x.style.display="block";
-  }
-}
